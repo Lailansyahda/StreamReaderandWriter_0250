ee7462a6806b11c909eb6e751889dac60ab6464d
Lailansyahda <syahdaazalia123@gmail.com>
2025-06-09 18:57:29 +0700

    try

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -12,6 +12,9 @@ int main()
     {
         array<int, 3> data = {10,20,30};
         //pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        //memanggil array elemen ke 4
     }
+    
     
 }
