eca616f6900719653bba3c7a33ecabfdbddd3b88
Lailansyahda <syahdaazalia123@gmail.com>
2025-06-09 18:24:30 +0700

    while looping

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -17,4 +17,11 @@ int main()
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "- ";
+        
+    }
 }
